199ffb4148d7118c946c2c75f3ad5095cbbe9b7b
hiluluke <hiluluke@hotmail.com>
2018-03-12 22:26:17 +0800

    Add postcss & autoprefixer

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -20,7 +20,7 @@ module.exports = () => {
           test: /\.(s)*css$/,
           use: ExtractTextPlugin.extract({
             fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
+            use: ['css-loader', 'sass-loader', 'postcss-loader']
           })
         }
       ]
